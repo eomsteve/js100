10d4995c39c5e55e7d5aa1abffece05886481d67
엄성현 <mingyov0807@gmail.com>
2023-01-01 23:50:03 +0900

    js100제 8번

---
+++ 008.js
@@ -0,0 +1,14 @@
+var d = {
+  'height':180,
+  'weight':78,
+  'weight':84,
+  'temperature':36,
+  'eyesight':1
+};
+
+console.log(d['weight']);
+
+/**
+ * 출력값은 : 84
+ * 객체에서 키가 중복될때 가장 나중에 할당된 값을 따른다.
+ */
