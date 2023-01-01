ac85db48e00fb36cc3bc28b697b5d51b2e9935fd
엄성현 <mingyov0807@gmail.com>
2023-01-01 23:28:42 +0900

    js100제 1번

---
+++ 001.js
@@ -0,0 +1,13 @@
+/**
+ * 다음 배열에서 400, 500를 삭제하는 code를 입력하세요.
+
+var numbs = [100, 200, 300, 400, 500];
+ */
+
+let numbs = [100, 200, 300, 400, 500];
+
+numbs.pop();
+numbs.pop();
+
+console.log(numbs)
+
